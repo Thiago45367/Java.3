e25ae96713ff2f9bd2a6e24c215ae74d1106ee5c
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-02-05 13:59:31 +0000

    salvar

---
+++ Variavel.java
@@ -0,0 +1,15 @@
+public class Variavel {
+    
+
+    public static void main(String[] args) {
+        
+        String nome="Anderson";
+        int idade=25;
+        double altura=1.80;
+            /*/
+            sytem.out.println(nome);
+            sytem.out.println(idade);
+            sytem.out.println(altura);*/
+            System.out.println("ola: "+nome+"sua idade é"+idade+"e sua altura"+altura+"");
+    }
+}
