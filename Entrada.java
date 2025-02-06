fa14a774b250e88f0f45a0101d74c79f69e11246
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-02-06 11:20:50 +0000

    Java.3

---
+++ Entrada.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class Entrada {
+    
+    public static void main(String[] args) {
+          
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Digite seu nome ");
+            String nome=sc.next();
+            System.out.println("Digite sua idade ");
+            double idade=sc.nextDouble();
+            System.out.println("Olá "+nome+" sua idade é "+idade);
+
+        sc.close();
+    }
+}
