ca26eea6db002b2da1bb4d115198a46795b050f8
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-02-10 11:26:39 +0000

    Java.3

--- Atividade5.java
+++ Atividade5.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class Atividade5 {
+
+    public static void main(String[] args) {
+        
+    String professor="";
     Scanner sc = new Scanner(System.in);
-    
+    while (!professor.equals("Anderson") ){
+        System.out.println("Qual o professor mais fofo do senai?");
+        professor=sc.next();
+        }
+    }
 }
---
+++ Atividade6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Atividade6 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcao=0;
+        while (opcao!=4) {
+            System.out.println("\n=== Menu ===");
+            System.out.println("1. opção 1");
+            System.out.println("2. opção 2");
+            System.out.println("3. opção 3");
+            System.out.println("4. Sair");
+            System.out.println("Escolha uma opção: ");
+
+            opcao=sc.nextInt();
+
+
+            
+        }
+    }
+}
