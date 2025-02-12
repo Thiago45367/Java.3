c32a412c7d74bae1c5fc2ac12845a2e9024597f2
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-02-12 12:04:49 +0000

    Java.3

--- AtividadeMenu1.java
+++ AtividadeMenu1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AtividadeMenu {
+public class AtividadeMenu1 {
     
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
