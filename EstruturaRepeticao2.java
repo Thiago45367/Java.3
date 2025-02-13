67e032e7b5a16aa1777b6384663e0ff0cde79727
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-02-13 11:53:36 +0000

    Java.3

---
+++ EstruturaRepeticao2.java
@@ -0,0 +1,11 @@
+public class EstruturaRepeticao2 {
+    
+    public static void main(String[] args) {
+        System.out.println("\n Números Impares: ");
+    for(int i=1;i<=100;i++){
+        if(i%2!=0){
+            System.out.println("Contagem"+i);
+        }
+    }
+    }
+}
